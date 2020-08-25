d84665983a0c444349e4d16d486bb086efbc31ab
Duchetta <cduchetta@minsait.com>
2020-08-25 14:45:05 +0200

    update application constant

--- ga_businessprocess/src/dossier/src/main/java/com/enel/config/ApplicationConstants.java
+++ ga_businessprocess/src/dossier/src/main/java/com/enel/config/ApplicationConstants.java
@@ -13,18 +13,18 @@ public final class ApplicationConstants {
 
 	public static final String API_DOCUMENTATION_TITLE = "Grid Activity API";
 	public static final String API_DOCUMENTATION_DESCRITPTION = "<h3>General</h3>"
-            + "<p>Dossier component, thru a set of REST calls, "
-            + "offers the chance to manage a set of \"Dossier \" intended to enrich Enel portfolio.</p>";
-	public static final String API_DOCUMENTATION_TAGS = "Dossiers";
+            + "<p>Model Entity component, thru a set of REST calls, "
+            + "offers the chance to manage a set of \"Model Entity \" intended to enrich Enel portfolio.</p>";
+	public static final String API_DOCUMENTATION_TAGS = "Model Entities";
 	public static final String API_STANDARD_JSON_FORMAT = "application/json; charset=UTF-8";
-    public static final String API_DOCUMENTATION_OPERATION = "Create a \"Dossier\" Resource";
+    public static final String API_DOCUMENTATION_OPERATION = "Create a \"Model Entity\" Resource";
     public static final String API_DOCUMENTATION_OPERATION_NOTES = "Returns the URL of the new resource in the Location header.";
 	public static final String API_PACKAGE_DEFINITION = "com.enel.controller";
 	
 	
 	/** ############################# DOSSIER DB SEQUENCE PARAMS ####################################### */
 		
-	public static final String MODEL_ENTITY_SEQUENCE_NAME = "dossier_sequence"; 
+	public static final String MODEL_ENTITY_SEQUENCE_NAME = "modelEntity_sequence"; 
 	public static final String MODEL_ENTITY_SEQUENCE_START = "1000000";
 	public static final String MODEL_ENTITY_SEQUENCE_INCREMENT_SIZE = "1";
 	
